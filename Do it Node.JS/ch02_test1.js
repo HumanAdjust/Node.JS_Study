6fbdb6bb4e0ce138329ef1357060a2924ca64822
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2019-04-13 22:03:20 +0900

    Update ch02_test1.js

--- Do it Node.JS/ch02_test1.js
+++ Do it Node.JS/ch02_test1.js
@@ -1,2 +1,9 @@
-console.log('현재 실행한 파일의 이름: %s', __filename);
-console.log('현재 실행한 파일의 패스: %s', __dirname)
+console.log('현재 실행한 파일의 이름입니다: %s', __filename);
+
+console.log('현재 실행한 파일의 경로입니다: %s', __dirname);
+
+//sefira라는 객체를 만들어 속성으로는 비나와 데이터 말소를 추가합니다.
+var sefira = {name:"비나", age:"[Data Erasured]"};
+
+//객체 안에 있는 모든 속성을 출력합니다.
+console.dir(sefira);
